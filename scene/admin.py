bd5dab37bccfb8c191faaca928e69bd34d2eab73
Felipe Hernández <cephei.1313@gmail.com>
2017-05-02 21:46:49 +0000

    added myadmin app to installed app

--- scene/admin.py
+++ scene/admin.py
@@ -2,7 +2,5 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.contrib.auth.models import Group
 
 # Register your models here.
-admin.site.unregister(Group)
