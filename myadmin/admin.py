7c9b60667dd77cf1e908d6f8f9c181f30abc57c6
Felipe Hernández <cephei.1313@gmail.com>
2017-05-02 21:45:40 +0000

    add myadmin app to set all admin options

---
+++ myadmin/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+# Register your models here.
+admin.site.unregister(Group)
