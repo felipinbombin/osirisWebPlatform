0cc2e2642b4fe1b2a7d3e2d1ed419cce5e7f6ed3
Felipe Hernández <cephei.1313@gmail.com>
2017-06-16 14:52:12 +0000

    added new warnings to upload file

--- scene/statusResponse.py
+++ scene/statusResponse.py
@@ -3,9 +3,15 @@ from django.http import JsonResponse
 
 class Status():
     OK = 1
+    INVALID_SIZE_FILE = 2
+    INVALID_STEP = 3
+    INVALID_FORMAT_FILE = 2
 
     statusDict = {
-        OK: {'code': 200, 'title': 'Consulta exitosa', 'message': ':-)', 'type': 'success'}
+        OK: {'code': 200, 'title': 'Consulta exitosa', 'message': ':-)', 'type': 'success'},
+        INVALID_SIZE_FILE: {'code': 401, 'title': 'Tamaño de archivo no válido', 'message': 'El archivo ha superado el espacio permitido.', 'type': 'error'},
+        INVALID_STEP: {'code': 402, 'title': 'Paso no permitido', 'message': 'Debe completar los pasos previos.', 'type': 'error'},
+        INVALID_FORMAT_FILE: {'code': 403, 'title': 'Formato de archivo no válido', 'message': 'El archivo debe tener formato Excel.', 'type': 'error'},
     }
 
     @staticmethod
--- scene/urls.py
+++ scene/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from .views.defaults import StepsView, ValidationStepView, GetStep1DataView
+from .views.UploadFile import UploadTopologicFileView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
@@ -9,6 +10,6 @@ urlpatterns = [
       login_required(ValidationStepView.as_view()), name='validation'),
     url(r'^scene/wizard/getStep1Data/(?P<sceneId>[0-9]+)$', 
       login_required(GetStep1DataView.as_view()), name='getStep1Data'),
-    
-#    url(r'^$', RoutePlanner.as_view()),
+    url(r'^scene/wizard/uploadTopologicFile/(?P<sceneId>[0-9]+)$', 
+      login_required(UploadTopologicFileView.as_view()), name='uploadTopologicFile'),
 ]
