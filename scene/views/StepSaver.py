939fa9fecd6805248b7b73c7502a28808a695162
Felipe Hernández <cephei.1313@gmail.com>
2017-08-08 16:03:29 -0400

    added transaction when edit or create topologic params

--- scene/views/StepSaver.py
+++ scene/views/StepSaver.py
@@ -1,10 +1,12 @@
 
-import uuid
+from django.db import transaction, IntegrityError
 
 from abc import ABCMeta, abstractmethod
 from .ExcelWriter import Step1ExcelWriter, Step3ExcelWriter, Step5ExcelWriter
 from scene.models import MetroConnection, MetroLine, MetroStation, MetroDepot, MetroConnectionStation, SystemicParams, \
     OperationPeriod
+
+import uuid
 
 
 class StepSaver:
@@ -36,96 +38,101 @@ class Step0Saver(StepSaver):
     def save(self, data):
         super(Step0Saver, self).save(data)
 
-        # all records all old by default
-        self.scene.metroline_set.update(isOld=True)
-        MetroStation.objects.filter(metroLine__scene=self.scene).update(isOld=True)
-        MetroDepot.objects.filter(metroLine__scene=self.scene).update(isOld=True)
-        MetroConnection.objects.filter(scene=self.scene).update(isOld=True)
-        MetroConnectionStation.objects.filter(metroConnection__scene=self.scene).\
-            update(isOld=True)
+        try:
+            with transaction.atomic():
+                # all records all old by default
+                self.scene.metroline_set.update(isOld=True)
+                MetroStation.objects.filter(metroLine__scene=self.scene).update(isOld=True)
+                MetroDepot.objects.filter(metroLine__scene=self.scene).update(isOld=True)
+                MetroConnection.objects.filter(scene=self.scene).update(isOld=True)
+                MetroConnectionStation.objects.filter(metroConnection__scene=self.scene).\
+                    update(isOld=True)
 
-        for line in data['lines']:
-            externalId = line['id']
-            name = line['name']
-            if externalId:
-                lineObj = MetroLine.objects.get(scene=self.scene, externalId=externalId)
-                lineObj.name = name
-                lineObj.isOld = False
-                lineObj.save()
-            else:
-                lineObj = MetroLine.objects.create(scene=self.scene, name=name,
-                                                   externalId=uuid.uuid4())
+                for line in data['lines']:
+                    externalId = line['id']
+                    name = line['name']
+                    if externalId:
+                        lineObj = MetroLine.objects.get(scene=self.scene, externalId=externalId)
+                        lineObj.name = name
+                        lineObj.isOld = False
+                        lineObj.save()
+                    else:
+                        lineObj = MetroLine.objects.create(scene=self.scene, name=name,
+                                                           externalId=uuid.uuid4())
 
-            for station in line['stations']:
-                if station['id']:
-                    MetroStation.objects.filter(metroLine=lineObj, externalId=station['id']). \
-                        update(name=station['name'], isOld=False)
-                else:
-                    MetroStation.objects.create(metroLine=lineObj, name=station['name'], externalId=uuid.uuid4())
+                    for station in line['stations']:
+                        if station['id']:
+                            MetroStation.objects.filter(metroLine=lineObj, externalId=station['id']). \
+                                update(name=station['name'], isOld=False)
+                        else:
+                            MetroStation.objects.create(metroLine=lineObj, name=station['name'], externalId=uuid.uuid4())
 
-            for depot in line['depots']:
-                if depot['id']:
-                    MetroDepot.objects.filter(metroLine=lineObj, externalId=depot['id']). \
-                        update(name=depot['name'], isOld=False)
-                else:
-                    MetroDepot.objects.create(metroLine=lineObj, name=depot['name'], externalId=uuid.uuid4())
+                    for depot in line['depots']:
+                        if depot['id']:
+                            MetroDepot.objects.filter(metroLine=lineObj, externalId=depot['id']). \
+                                update(name=depot['name'], isOld=False)
+                        else:
+                            MetroDepot.objects.create(metroLine=lineObj, name=depot['name'], externalId=uuid.uuid4())
 
-        for connection in data['connections']:
-            # global connections
-            externalId = connection['id']
-            avgHeight = float(connection['avgHeight'])
-            avgSurface = float(connection['avgSurface'])
-            if externalId:
-                connectionObj = MetroConnection.objects.prefetch_related('stations'). \
-                    get(scene=self.scene, externalId=externalId)
-                connectionObj.name = connection['name']
-                connectionObj.avgHeight = avgHeight
-                connectionObj.avgSurface = avgSurface
-                connectionObj.isOld = False
-                connectionObj.save()
-            else:
-                connectionObj = MetroConnection.objects.create(scene=self.scene, name=connection['name'],
-                                                               avgHeight=avgHeight, avgSurface=avgSurface,
-                                                               externalId=uuid.uuid4())
+                for connection in data['connections']:
+                    # global connections
+                    externalId = connection['id']
+                    avgHeight = float(connection['avgHeight'])
+                    avgSurface = float(connection['avgSurface'])
+                    if externalId:
+                        connectionObj = MetroConnection.objects.prefetch_related('stations'). \
+                            get(scene=self.scene, externalId=externalId)
+                        connectionObj.name = connection['name']
+                        connectionObj.avgHeight = avgHeight
+                        connectionObj.avgSurface = avgSurface
+                        connectionObj.isOld = False
+                        connectionObj.save()
+                    else:
+                        connectionObj = MetroConnection.objects.create(scene=self.scene, name=connection['name'],
+                                                                       avgHeight=avgHeight, avgSurface=avgSurface,
+                                                                       externalId=uuid.uuid4())
 
-            for connectionStation in connection['stations']:
-                station = connectionStation['station']
-                line = connectionStation['line']
+                    for connectionStation in connection['stations']:
+                        station = connectionStation['station']
+                        line = connectionStation['line']
 
-                if station['id']:
-                    stationObj = MetroStation.objects.get(metroLine__scene=self.scene, externalId=station['id'])
-                else:
-                    stationObj = MetroStation.objects.get(metroLine__name=line['name'],
-                                                          metroLine__scene=self.scene, name=station['name'])
+                        if station['id']:
+                            stationObj = MetroStation.objects.get(metroLine__scene=self.scene, externalId=station['id'])
+                        else:
+                            stationObj = MetroStation.objects.get(metroLine__name=line['name'],
+                                                                  metroLine__scene=self.scene, name=station['name'])
 
-                if connectionStation['id']:
-                    MetroConnectionStation.objects.filter(metroConnection=connectionObj,
-                                                          externalId=connectionStation['id']).update(
-                        metroStation=stationObj, isOld=False)
-                else:
-                    MetroConnectionStation.objects.create(metroConnection=connectionObj,
-                                                          metroStation=stationObj, externalId=uuid.uuid4())
+                        if connectionStation['id']:
+                            MetroConnectionStation.objects.filter(metroConnection=connectionObj,
+                                                                  externalId=connectionStation['id']).update(
+                                metroStation=stationObj, isOld=False)
+                        else:
+                            MetroConnectionStation.objects.create(metroConnection=connectionObj,
+                                                                  metroStation=stationObj, externalId=uuid.uuid4())
 
-        MetroLine.objects.filter(scene=self.scene, isOld=True).delete()
-        MetroStation.objects.filter(metroLine__scene=self.scene, isOld=True).delete()
-        MetroDepot.objects.filter(metroLine__scene=self.scene, isOld=True).delete()
-        MetroConnection.objects.filter(scene=self.scene, isOld=True).delete()
-        MetroConnectionStation.objects.filter(metroConnection__scene=self.scene, isOld=True).delete()
+                MetroLine.objects.filter(scene=self.scene, isOld=True).delete()
+                MetroStation.objects.filter(metroLine__scene=self.scene, isOld=True).delete()
+                MetroDepot.objects.filter(metroLine__scene=self.scene, isOld=True).delete()
+                MetroConnection.objects.filter(scene=self.scene, isOld=True).delete()
+                MetroConnectionStation.objects.filter(metroConnection__scene=self.scene, isOld=True).delete()
 
-        # move to next step
-        if self.scene.currentStep == 0:
-            self.scene.currentStep = 1
-            self.scene.save()
+                # move to next step
+                if self.scene.currentStep == 0:
+                    self.scene.currentStep = 1
+                    self.scene.save()
 
-        # create template file for step 1
-        step1Excel = Step1ExcelWriter(self.scene)
-        step1Excel.createTemplateFile()
+                # create template file for step 1
+                step1Excel = Step1ExcelWriter(self.scene)
+                step1Excel.createTemplateFile()
 
-        # create template file for step 3
-        step3Excel = Step3ExcelWriter(self.scene)
-        step3Excel.createTemplateFile()
+                # create template file for step 3
+                step3Excel = Step3ExcelWriter(self.scene)
+                step3Excel.createTemplateFile()
 
-        return True
+                return True
+        except IntegrityError:
+            return False
+
 
 class Step2Saver(StepSaver):
     """
