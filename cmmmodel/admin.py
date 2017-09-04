83c7fedcd904a4622499c09328dd6ed23b465204
Felipe Hernández <cephei.1313@gmail.com>
2017-09-03 21:19:07 -0300

    added model to admin to debug

--- cmmmodel/admin.py
+++ cmmmodel/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Register your models here.
+from django.contrib import admin
+from .models import ModelExecutionHistory, ModelExecutionQueue, Model, PossibleQueue
+
+admin.site.register(Model)
+admin.site.register(ModelExecutionHistory)
+admin.site.register(ModelExecutionQueue)
+admin.site.register(PossibleQueue)
