b801b55ad0d6038d8ace0094a24eada3635d96df
Felipe Hernández <cephei.1313@gmail.com>
2017-09-24 10:38:45 -0300

    WIP: working on speed model view. Fixed url

--- viz/urls.py
+++ viz/urls.py
@@ -3,10 +3,10 @@ from .views import SpeedModelViz, SpeedModelVizData
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url(r'^viz/speed/(?P<sceneId>[0-9]+)$',
+    url(r'^speed/(?P<sceneId>[0-9]+)$',
       login_required(SpeedModelViz.as_view()), name='speedModel'),
-    url(r'^viz/speed/data/(?P<sceneId>[0-9]+)$',
+    url(r'^speed/data/(?P<sceneId>[0-9]+)$',
       login_required(SpeedModelVizData.as_view()), name='speedModelData'),
-    url(r'^viz/strong/(?P<sceneId>[0-9]+)$',
+    url(r'^strong/(?P<sceneId>[0-9]+)$',
       login_required(SpeedModelViz.as_view()), name='strongModel')
 ]
