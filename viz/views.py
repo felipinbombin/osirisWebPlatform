81a62c23445a4c849c1dc828e1b5619db9c79e7b
Felipe Hernández <cephei.1313@gmail.com>
2017-09-24 15:50:50 -0300

    WIP: parser output model

--- viz/views.py
+++ viz/views.py
@@ -39,9 +39,19 @@ class SpeedModelVizData(View):
         answer = ModelAnswer.objects.filter(execution=execution).values_list("metroLine__name", "direction",
                                                                              "metroTrack__name",
                                                                              "operationPeriod__name", "attributeName",
-                                                                             "order", "value")
+                                                                             "value").\
+            order_by("metroLine__name", "direction", "metroTrack__name", "operationPeriod__name", "attributeName", "order")
+        from itertools import groupby
+        groups = []
+        for k1, g1 in groupby(answer, 0):
+            for k2, g2 in groupby(g1, 1):
+                for k3, g3 in groupby(g2, 2):
+                    for k4, g4 in groupby(g3, 3):
+                        for k5, g5 in groupby(g4, 4):
+                            groups = list(g5)
+
         response = {}
-        response["answer"] = []
+        response["answer"] = groups
 
         for row in answer:
             response["answer"].append(row)
