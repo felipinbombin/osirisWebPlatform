4fea48d4f84976a49497e111a100648cc417c267
Felipe Hernández <cephei.1313@gmail.com>
2017-10-31 17:19:00 -0300

    refactoring

--- viz/views/strong.py
+++ viz/views/strong.py
@@ -31,8 +31,10 @@ class StrongModelViz(View):
         self.context["scene"] = scene_obj
         self.context["metro_lines"] = [metro_line_obj.name for metro_line_obj in
                                        scene_obj.metroline_set.all().order_by("id")]
-        self.context["directions"] = [scene_obj.metroline_set.all().order_by("id")[0].get_direction_name(direction) for direction
-                                      in [MetroLineMetric.GOING, MetroLineMetric.REVERSE]]
+        self.context["directions"] = [
+            {"value": direction, "item": scene_obj.metroline_set.all().order_by("id")[0].get_direction_name(direction)}
+            for direction
+            in [MetroLineMetric.GOING, MetroLineMetric.REVERSE]]
         self.context["op_periods"] = [{"value": op_period_obj.name,
                                        "item": "{} ({} - {})".format(op_period_obj.name, op_period_obj.start,
                                                                      op_period_obj.end)} for op_period_obj in
