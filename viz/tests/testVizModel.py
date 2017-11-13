80dbe1c2bb7ac9e1527f50b9464fa4938d1ac4de
Felipe Hernández <cephei.1313@gmail.com>
2017-11-12 22:38:41 -0300

    improved case coverage for javascript code

--- viz/tests/testVizModel.py
+++ viz/tests/testVizModel.py
@@ -17,6 +17,7 @@ import uuid
 import pickle
 import os
 import json
+import time
 
 
 class ModelVizTest(TestCase):
@@ -224,6 +225,72 @@ class JavascriptTest(StaticLiveServerTestCase):
         browser.fill("password", self.password)
         browser.find_by_value("Iniciar sesión").click()
 
+    def go_next_step(self, browser):
+        """ move to next step """
+        time.sleep(0.5)
+        browser.find_by_text("Guardar y avanzar").click()
+
+    def complete_step_0(self, browser):
+        """ fill all data for step 0 """
+        line_names = ["L1", "L2"]
+        station_quantities = [10, 12]
+
+        connection_name = "ConnectionL1L2"
+        avg_height = "7.2"
+        avg_surface = "4300"
+        connection_stations = ["S5", "S6"]
+
+        # first metro line
+        time.sleep(0.5)
+        browser.find_by_id("addLine").click()
+        time.sleep(0.5)
+        browser.find_by_id("stationQuantity").fill(station_quantities[0])
+        browser.find_by_id("createLine").click()
+
+        # second metro line
+        time.sleep(0.5)
+        browser.find_by_id("addLine").click()
+        time.sleep(0.5)
+        browser.find_by_id("stationQuantity").fill(station_quantities[1])
+        browser.find_by_id("createLine").click()
+
+        # create depots
+        time.sleep(0.5)
+        browser.find_by_id("addDepot").click()
+        time.sleep(0.5)
+        browser.find_by_id("depotLineName")
+        browser.find_option_by_text(line_names[0]).click()
+        browser.find_by_id("createDepot").click()
+
+        time.sleep(0.5)
+        browser.find_by_id("addDepot").click()
+        time.sleep(0.5)
+        browser.find_option_by_text(line_names[1]).click()
+        browser.find_by_id("createDepot").click()
+
+        # create connection
+        time.sleep(0.5)
+        browser.find_by_id("addConnection").click()
+        time.sleep(0.5)
+        browser.find_by_id("connectionName").fill(connection_name)
+        browser.find_by_id("avgHeight").fill(avg_height)
+        browser.find_by_id("avgSurface").fill(avg_surface)
+
+        browser.find_by_id("addConnectionStation").click()
+        browser.find_by_id("addConnectionStation").click()
+
+        for index, connection_station_name in enumerate(connection_stations):
+            browser.select_by_text("connectionStationLine%s" % index, line_names[index])
+            browser.select_by_text("connectionStationName%s" % index, connection_station_name)
+
+        browser.find_by_id("createConnection").click()
+
+    def complete_step_1(self, browser):
+        """  upload topologic file """
+        file_path = os.path.join(settings.BASE_DIR, "scene", "tests", "Escenario_topologico.xlsx")
+        browser.find_by_id("step2form").click()
+        browser.attach_file("file", file_path)
+
     def create_scene(self, browser):
         """ create scene through webpage """
 
@@ -234,21 +301,12 @@ class JavascriptTest(StaticLiveServerTestCase):
         browser.find_by_name("_save").click()
 
         # create topologic variables
+        self.complete_step_0(browser)
+        self.go_next_step(browser)
+        self.complete_step_1(browser)
+        self.go_next_step(browser)
 
-        import time
-        time.sleep(3)
-        # first line
-        browser.find_by_id("addLine").click()
-        browser.find_by_id("stationQuantity").fill(10)
-        browser.find_by_id("createLine").click()
-
-        # second line
-        browser.find_by_id("addLine").click()
-        browser.find_by_id("stationQuantity").fill(12)
-        browser.find_by_id("createLine").click()
-
-        #browser.find_by_id("addDepot").click()
-
+        time.sleep(30)
         #browser.find_by_id("addConnection").click()
 
     def test_loadChart(self):
