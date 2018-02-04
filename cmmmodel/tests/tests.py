82de9e245a505a4e6898053ae9ae01a18432ea1c
Felipe Hernández <cephei.1313@gmail.com>
2018-02-04 11:51:38 -0300

    added test to process heat model answer
    Former-commit-id: 3ade6d9fb62fbffc38170962c270957aac74d905

--- cmmmodel/tests/tests.py
+++ cmmmodel/tests/tests.py
@@ -242,3 +242,19 @@ class ExecuteModel(TestCase):
             process_answer(answer, execution_obj)
 
         self.assertEqual(ModelAnswer.objects.count(), 21)
+
+    def test_processHeatAnswer(self):
+        """ test load data from heat model output dict based on situation of file """
+        file_name = "heat.model_output"
+        file_path = os.path.join("cmmmodel", "tests", file_name)
+
+        self.create_topologic_system()
+
+        execution_obj = ModelExecutionHistory.objects.create(scene=self.scene_obj, model_id=Model.THERMAL_MODEL_ID,
+                                                             externalId=uuid.uuid4(), start=timezone.now())
+        with open(file_path, "rb") as answer_file:
+            answer = pickle.load(answer_file)
+            answer = answer["output"]
+            process_answer(answer, execution_obj)
+
+        self.assertEqual(ModelAnswer.objects.count(), 21)
