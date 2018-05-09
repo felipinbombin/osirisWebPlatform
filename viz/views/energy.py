c75366dd04f72e986f2e4ec68afd3cebd588fa54
Felipe Hernández <cephei.1313@gmail.com>
2018-05-09 13:56:22 -0300

    added logic to retrieve data from energy center model

--- viz/views/energy.py
+++ viz/views/energy.py
@@ -13,8 +13,9 @@ from scene.statusResponse import Status
 
 from cmmmodel.models import ModelExecutionHistory, CMMModel
 from cmmmodel.transform.processEnergyData import ProcessEnergyData
+from cmmmodel.transform.processEnergyCenterData import ProcessEnergyCenterData
 
-from viz.models import ModelAnswer
+from viz.models import ModelAnswer, EnergyCenterModelAnswer
 
 
 class EnergyModelViz(View):
@@ -37,6 +38,11 @@ class EnergyModelViz(View):
                 "value": key,
                 "item": _(ProcessEnergyData.dictionary_group[key])
             })
+        for key in ProcessEnergyCenterData.dictionary_group.keys():
+            charts.append({
+                "value": key,
+                "item": _(ProcessEnergyData.dictionary_group[key])
+            })
         self.context["charts"] = charts
         self.context["execution_obj"] = ModelExecutionHistory.objects.filter(scene=scene_obj,
                                                                              model_id=CMMModel.ENERGY_MODEL_ID). \
@@ -48,11 +54,30 @@ class EnergyModelViz(View):
 class EnergyModelVizData(View):
     """ data for charts  """
 
-    def get_model_data(self, execution_obj, prefix):
+    def get_energy_model_data(self, execution_obj, prefix):
         """ get data have gotten from execution instance """
 
-        prefix = ProcessEnergyData.dictionary_group[prefix]
         answer = ModelAnswer.objects.prefetch_related(). \
+            filter(execution=execution_obj, attributeName__startswith=prefix).values_list("attributeName", "value"). \
+            order_by("attributeName", "order")
+
+        groups = []
+        for key, group in groupby(answer, lambda row: "{}".format(row[0].split("_")[0])):
+            # group by key
+            group_element = {
+                "prefix": key,
+                "attributes": {}
+            }
+            for key2, value in group:
+                code = key2.split("_")[1]
+                group_element["attributes"][_(code)] = value
+            groups.append(group_element)
+
+        return groups
+
+    def get_energy_center_model_data(self, execution_obj, prefix):
+
+        answer = EnergyCenterModelAnswer.objects.prefetch_related(). \
             filter(execution=execution_obj, attributeName__startswith=prefix).values_list("attributeName", "value"). \
             order_by("attributeName", "order")
 
@@ -80,10 +105,17 @@ class EnergyModelVizData(View):
 
         # attributes to retrieve
         prefix = request.GET.get("prefix", "")
-
         scene_id = int(scene_id)
-        execution_obj = ModelExecutionHistory.objects.filter(scene_id=scene_id, model_id=CMMModel.ENERGY_MODEL_ID). \
-            order_by("-id").first()
+
+        try:
+            prefix = ProcessEnergyData.dictionary_group[prefix]
+            execution_obj = ModelExecutionHistory.objects.filter(scene_id=scene_id, model_id=CMMModel.ENERGY_MODEL_ID). \
+                order_by("-id").first()
+        except KeyError:
+            prefix = ProcessEnergyCenterData.dictionary_group[prefix]
+            execution_obj = ModelExecutionHistory.objects.filter(scene_id=scene_id,
+                                                                 model_id=CMMModel.ENERGY_CENTER_MODEL_ID). \
+                order_by("-id").first()
 
         response = {}
         if execution_obj is None:
@@ -91,6 +123,9 @@ class EnergyModelVizData(View):
         elif execution_obj.status != ModelExecutionHistory.OK:
             Status.getJsonStatus(Status.LAST_MODEL_FINISHED_BADLY_ERROR, response)
         else:
-            response["answer"] = self.get_model_data(execution_obj, prefix)
+            if execution_obj.id == CMMModel.ENERGY_MODEL_ID:
+                response["answer"] = self.get_energy_model_data(execution_obj, prefix)
+            else:
+                response["answer"] = self.get_energy_center_model_data(execution_obj, prefix)
 
         return JsonResponse(response, safe=False)
