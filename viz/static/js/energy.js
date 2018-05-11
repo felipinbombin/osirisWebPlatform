300bae65f620a2372af8a2e8bc368734e9f80d9a
Felipe Hernández <cephei.1313@gmail.com>
2018-05-11 00:21:30 -0300

    refactoring

--- viz/static/js/energy.js
+++ viz/static/js/energy.js
@@ -229,6 +229,7 @@ $(document).ready(function () {
             lineChart.setOption(lineOptions, {
                 notMerge: true
             });
+            lineChart.resize();
         };
 
         var makeAjaxCall = true;
